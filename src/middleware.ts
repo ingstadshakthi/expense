1d379797196b7c092c4ce756ab54150317e27f81
ingstadshakthi <ingstad26@gmail.com>
2025-09-18 23:36:38 +0530

    feat(auth): added auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,5 +2,5 @@ import { auth } from "@/lib/auth";
 
 export const middleware = auth;
 export const config = {
-  matcher: ["/dashboard", "/profile"],
+  matcher: ["/dashboard", "/profile", "/profile/expense", "/profile/payment"],
 };
